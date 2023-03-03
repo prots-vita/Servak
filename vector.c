b8b913ac589032b6b23e656324ca2a772f520ec2
prots-vita <prots-vita@mail.ru>
2023-03-03 17:30:49 +0300

    bibl of vector

--- vector.c
+++ vector.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "vector.h"
 
-typedef struct Space{
-	int *arr;
-	int size;
-	int tail;
-}Space;
-
-int queue(Space *, int); //ADD AN ELEMENT
-int pop(Space *); //DELETE AN ELEMENT
-int isEmpty(Space *); //EMPTY - 1, NOT EMPTY - 0
-int isFull(Space *); //FULL - 1, NOT FULL - 0
-int peek(Space *); //GET AN ELEMENT FROM THE BEGIN
+Space* creat(Space *sp){
+	sp->arr = malloc(12*sizeof(int));
+	sp->size = 12;
+	sp->tail = -1;
+}	
 
 int queue(Space *sp, int elem){
 	if (isFull(sp)){
@@ -62,30 +57,9 @@ void print(Space *sp){
 	printf("\n");
 }
 
-int main(){
-	srand(time(NULL));
-	Space space;
-	space.arr = malloc(12*sizeof(int));
-	space.size = 12;
-	space.tail = -1;
-	for (int i = 1; i<40; i++){
-		switch(rand()%2){
-			case 2:
-				if (queue(&space, i)){
-					printf("QUEUE\n");
-					print(&space);
-				}
-			case 0:
-				if (pop(&space)){
-					printf("POP\n");
-					print(&space);
-				}
-			case 1:
-				if (!isEmpty(&space)){
-					printf("PEEK\n");
-					printf("%d\n", peek(&space));
-				}
-		}
-	}
-
+void clear(Space *sp){
+	free(sp->arr);
+	sp->size = 0;
+	sp->tail = -1;
 }
+
